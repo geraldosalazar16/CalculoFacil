c92609a66eeb307985809018906ae41ba7e3372b
geraldosalazar16 <geraldosalazar16@gmail.com>
2018-10-11 18:11:00 +0200

    Leer desglose del resultado y mostrar los elementos

--- src/pages/resultados/resultados.ts
+++ src/pages/resultados/resultados.ts
@@ -1,12 +1,6 @@
+//Importando componentes
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the ResultadosPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -14,13 +8,18 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'resultados.html',
 })
 export class ResultadosPage {
+  //Variables de uso general
   respuesta: any;
   explic: any;
   textos: string[];
   mostrar_procedimiento: boolean = true;
+  componentes_resultado: Componete_Resultado[] = [];
 
+  //Punto de entrada de la aplicación
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.respuesta = navParams.get('respuesta');
+
+    /* Inicio procesamiento explic */
     this.explic = this.respuesta.result.explic.proc;
     this.explic = JSON.stringify(this.explic);
     this.explic = this.escapeHtml(this.explic );
@@ -48,7 +47,38 @@ export class ResultadosPage {
       }
     });
     this.textos = filtro1;
+    /* Fin procesamiento explic */
+    /* Procesar resultado */
+    /*
+    Estructura del desglose
+      desglose= {
+        grupos: [
+          {
+            nombre: '',
+            datos: [
+              {
+                tipo: '',
+                valores: ['','']
+              }
+            ]
+          }
+        ]
+      }
+    */
+    var grupos = this.respuesta.desglose.grupos;
+    grupos.forEach(grupo => {
+      var datos = grupo.datos;
+      datos.forEach(dato => {
+        this.crear_elemento(<Dato>dato);
+      });
+    });
   }
+  /*
+  @quiero Función que elimina el caracter \ de un string   
+  @para ---- Procesar explic ya que viene mezclado el mensaje con código html
+  @author ---- Geraldo
+  @param ---- 
+  */
   escapeHtml(text) {
     var map = {
       '\\':' '
@@ -58,6 +88,12 @@ export class ResultadosPage {
     }
     return null;
   }
+  /*
+  @quiero Función que agrega tildes y ñ   
+  @para ---- Procesar explic ya que viene mezclado el mensaje con código html
+  @author ---- Geraldo
+  @param ---- 
+  */
   cambios(text){
     var map = {
       '&aacute;':'á',
@@ -74,5 +110,58 @@ export class ResultadosPage {
   }
   ionViewDidLoad() {
   }
+  crear_elemento(dato: Dato){
+    var nuevo_cmp = new Componete_Resultado;
+    nuevo_cmp.valor = [];
+    switch (dato.tipo) {
+      case 'encabezado':
+        nuevo_cmp = new Componete_Resultado;
+        nuevo_cmp.valor = [];
+        nuevo_cmp.tipo = 'encabezado';
+        nuevo_cmp.valor[0] = dato.valores[0];
+        this.componentes_resultado.push(nuevo_cmp);
+        break;
+      case 'normal':
+        nuevo_cmp = new Componete_Resultado;
+        nuevo_cmp.valor = [];
+        nuevo_cmp.tipo = 'normal';
+        nuevo_cmp.valor[0] = dato.valores[0]+' '+dato.valores[1];
+        this.componentes_resultado.push(nuevo_cmp);
+        break;
+      case 'uno_centrado':
+        nuevo_cmp = new Componete_Resultado;
+        nuevo_cmp.valor = [];
+        nuevo_cmp.tipo = 'uno_centrado';
+        nuevo_cmp.valor[0] = dato.valores[0];
+        this.componentes_resultado.push(nuevo_cmp);
+        break;
+      case 'uno':
+        nuevo_cmp = new Componete_Resultado;
+        nuevo_cmp.valor = [];
+        nuevo_cmp.tipo = 'uno';
+        nuevo_cmp.valor[0] = dato.valores[0];
+        this.componentes_resultado.push(nuevo_cmp);
+        break;
+      case 'negritas':
+        nuevo_cmp = new Componete_Resultado;
+        nuevo_cmp.valor = [];
+        nuevo_cmp.tipo = 'negritas';
+        nuevo_cmp.valor[0] = dato.valores[0];
+        nuevo_cmp.valor[1] = ' '+dato.valores[1];
+        this.componentes_resultado.push(nuevo_cmp);
+        break;
+      default:
+        
+        break;
+    }
+  }
+}
+export interface Dato{
+  tipo: string;
+  valores: string[];
+}
 
+export class Componete_Resultado{
+  tipo: string;
+  valor: string[];
 }
